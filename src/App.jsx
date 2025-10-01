fd617e3e1b34c3c6d852a0ca0a247a14e2727dad
Ryan Motchkavitz <ryanmotch2@gmail.com>
2025-10-01 17:54:57 -0400

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,73 @@
+import { episodeList } from "./data";
+import {useState} from "react"
+
 export default function App() {
-  // TODO
+  const [episodes] = useState(episodeList);
+    const [selectedEpisode, setSelctedEpisode] = useState();
+
+//selected details
+
+ function EpisodeDetails() {
+    if (!selectedEpisode) {
+      return (
+        <section className="details">
+          <h2>Episode Details</h2>
+          <p>Select an episode to learn more.</p>
+        </section>
+      );
+    }
+
+
+    return (
+      <section className="details">
+        <h2>{selectedEpisode.id}</h2>
+        <p>
+          {selectedEpisode.title}.
+        </p>
+        
+        <address>{selectedPuppy.description}</address>
+      </section>
+    );
+  }
+
+  //roster of episodes
+
+  function Roster() {
+    return (
+      <section className="roster">
+        <h2>Roster</h2>
+        <ul className="roster">
+          {episodes.map((episode) => (
+            <li key={epipsode.id} onClick={() => setSelectedEpisode(Episode)}>
+              {episode.name}
+            </li>
+          ))}
+        </ul>
+      </section>
+    );
+  }
+
+  return (
+    <>
+      <header>
+        <h1>Episodes</h1>
+      </header>
+      <main>
+        <Roster />
+        <EpisodeDetails />
+      </main>
+    </>
+  );
 }
+
+
+
+
+
+
+
+
+
+
+
+
